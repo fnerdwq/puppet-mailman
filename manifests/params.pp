fd64e995526b982bc91a14870b0c4b26f748a0e2
Frederik Wagner <wagner@wagit.de>
2014-09-18 12:00:20 +0200

    moved all parameters to params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,20 @@
 # set flavor specific variables (private)
 class mailman::params {
 
+  $url_pattern        = 'http://%s/cgi-bin/mailman/'
+  $email_host         = $::fqdn
+  $url_host           = $::fqdn
+  $server_language    = 'en'
+  $mta                = undef
+  $smtpport           = undef
+  $public_archive_url = undef
+  $site_list          = 'mailman'
+  $site_list_admin    = "root@${::fqdn}"
+  $site_list_pw       = 'initial'
+  $master_list_pw     = 'master'
+  $create_list_pw     = 'create'
+  $lists              = {}
+
   case $::osfamily {
     'Debian': {
       $group = 'list'
